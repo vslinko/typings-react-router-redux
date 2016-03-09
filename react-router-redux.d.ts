fd8281a7d199a93cd85e7c590a1f4d855f4427c6
Alexey Svetliakov <asvetliakov@teledini.com>
2016-03-10 00:59:32 +0100

    Modified for react-router-redux 4.0 Updated redux dep

--- react-router-redux.d.ts
+++ react-router-redux.d.ts
@@ -3,33 +3,55 @@
 import {History, Location, LocationDescriptor} from "history";
 import {IMiddleware, IStore, IAction, IReducer} from "redux";
 
-declare module reactRouterRedux {
-	export interface IRouterReduxMiddleware<TState> extends IMiddleware<TState> {
-		/**
-		 * By default, the syncing logic will not respond to replaying of actions, which means it won't work with projects like redux-devtools. Call this function on the middleware object returned from syncHistory and give it the store to listen to, and it will properly work with action replays.
-		 * Obviously, you would do that after you have created the store and everything else has been set up.
-		 * Supply an optional function selectLocationState to customize where to find the location state on your app state. It defaults to state => state.routing.location, so you would install the reducer under the name "routing". Feel free to change this to whatever you like.
-		 * 
-		 * @param {IStore<TState>} store (description)
-		 * @param {(state:TState) => Location} selectLocationState (description)
-		 */
-		listenForReplays(store: IStore<TState>, selectLocationState?: (state: TState) => Location);
+declare module reactRouterRedux {    
+    /**
+     * An action type that you can listen for in your reducers to be notified of route updates. Fires after any changes to history.
+     */
+    export const LOCATION_CHANGE: string;
+    
+    /**
+     * Router action
+     */
+    export interface RouterAction extends IAction {
+        /**
+         * New location
+         */
+        payload: Location;
+    }
 
-		/**
-		 * Call this on the middleware returned from syncHistory to stop the syncing process set up by listenForReplays.
-		 */
-		unsubscribe();
-	}
-
-	/**
-	 * Call this to create a middleware that can be applied with Redux's applyMiddleware to allow actions to call history methods. The middleware will look for route actions created by push, replace, etc. and applies them to the history.
-	 * 
-	 * @export
-	 * @template TState
-	 * @param {History} history (description)
-	 * @returns {IRouterReduxMiddleware<TState>} (description)
-	 */
-	export function syncHistory<TState>(history: History): IRouterReduxMiddleware<TState>;
+    /**
+     * Redux router internal state. Useful if you're overriding own reducer
+     */
+    export interface IRouterState {
+        locationBeforeTransitions: Location;
+    }
+    
+    /**
+     * History syncing options
+     */
+    interface ISyncHistoryOptions<TState> {
+        /**
+         * When false, the URL will not be kept in sync during time travel. 
+         * This is useful when using persistState from Redux DevTools and not wanting to maintain the URL state when restoring state.
+         * @default true
+         */
+        adjustUrlOnReplay?: boolean;
+        /**
+         * A selector function to obtain the history state from your store. 
+         * Useful when not using the provided routerReducer to store history state. Allows you to use wrappers, such as Immutable.js.
+         * @default state => state.routing
+         */
+        selectLocationState?: (state: TState) => IRouterState;
+    }
+    
+    /**
+     * @export
+     * @template TState
+     * @param {History} history History singleton from react-router
+     * @param {IStore<TState>} store Application store
+     * @param {ISyncHistoryOptions<TState>} options Syncing options
+     */
+    export function syncHistoryWithStore<TState>(history: History, store: IStore<TState>, options?: ISyncHistoryOptions<TState>): History;
 
 	/**
 	 * A reducer function that keeps track of the router state. You must add this reducer to your app reducers when creating the store. 
@@ -41,30 +63,99 @@ declare module reactRouterRedux {
 	 * @param {IAction} action (description)
 	 * @returns {Location} (description)
 	 */
-	export function routeReducer(state: Location, action: IAction): Location;
+	export function routeReducer(state: IRouterState, action: RouterAction): IRouterState;
+    
+        
+    /**
+     * A middleware you can apply to your Redux store to capture dispatched actions created by the action creators. 
+     * It will redirect those actions to the provided history instance.
+     */
+    export function routerMiddleware<TState>(history: History): IMiddleware<TState>;
+    
+    // Action types interfaces. These are deprecated
+    /**
+     * @deprecated
+     */
+    interface IRoutePushAction {
+        (nextLocation: LocationDescriptor): IAction;
+    }
+    
+    /**
+     * @deprecated
+     */
+    interface IRouteReplaceAction {
+        (nextLocation: LocationDescriptor): IAction;
+    }
+    
+    /**
+     * @deprecated
+     */
+    interface IRouteGoAction {
+        (n: number): IAction;
+    }
+    
+    /**
+     * @deprecated
+     */
+    interface IRouteGoForwardAction {
+        (): IAction;
+    }
+    
+    /**
+     * @deprecated
+     */
+    interface IRouteGoBackAction {
+        (): IAction;
+    }
 
+    /**
+     * @deprecated
+     */
 	export interface IRouteActions {
-		push(nextLocation: LocationDescriptor): IAction;
-		replace(nextLocation: LocationDescriptor): IAction;
-		go(n: number): IAction;
-		goForward(): IAction;
-		goBack(): IAction;
+		push: IRoutePushAction;
+		replace: IRouteReplaceAction;
+		go: IRouteGoAction;
+		goForward: IRouteGoForwardAction;
+		goBack: IRouteGoBackAction;
 	}
 
 	/**
 	 * An object that contains all the actions creators you can use to manipulate history:
-	 * 
+	 * @deprecated
 	 * @export
 	 */
 	export const routeActions: IRouteActions;
+    
+    /**
+     * This action type will be dispatched by the history actions below.
+     * If you're writing a middleware to watch for navigation events, be sure to
+     * look for actions of this type.
+     * @export
+     */
+    export const CALL_HISTORY_METHOD: string;
+    
+    // separate actions
+    /**
+     * @deprecated
+     */
+    export const push: IRoutePushAction;
+    /**
+     * @deprecated
+     */
+    export const replace: IRouteReplaceAction;
+    /**
+     * @deprecated
+     */
+    export const go: IRouteGoAction;
+    /**
+     * @deprecated
+     */
+    export const goForward: IRouteGoForwardAction;
+    /**
+     * @deprecated
+     */
+    export const goBack: IRouteGoBackAction;
 
-	/**
-	 * Action type for when the location is updated
-	 * @export
-	 */
-	export const UPDATE_LOCATION: string;
-
-	export const TRANSITION: string;
 }
 
 export = reactRouterRedux;
